7e83d150c656ce7d8f69009ba00a2c0ecac60a08
maliang11 <2635185063@qq.com>
2024-11-18 13:26:08 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,40 +1,56 @@
-//请在下方输入你的代码：
-void delay_ms(unsigned int x)  // 延时函数
-{
-    unsigned int i,j;
-    if(x==1000)
-    {
-        for(i=0;i<19601;i++)//延时1s
-        {
-            for(j=5;j>0;j--);
-        }
-    }
-    else while(x--)for(j=115;j>0;j--);
-}
-
-// tips: 原理图当中led为低电平点亮，比如点亮LED2,代码为： P0 = 0xFE (1111 1110)
-
-
+#include "stm32f10x.h"               
+#include "Delay.h"
+#include "OLED.h"              
+#include "Key.h"
 int main(void)
 {
-   while(1)
+	Key_Init();
+	OLED_Init();	
+	while (1)
+	{
+		swtich(KeyNum)
+		{  case 1:
+		   {
+            OLED_Clear();
+			Delay_ms(10);
+			OLED_ShowString(1, 1, "我靠NB");
+			break;
+           }
+           case 2:
+		   {
+			OLED_Clear();
+			Delay_ms(10);
+			OLED_ShowString(1, 1, "电协NB");
+			break;
+		   }
+            case 3:
+		   {  
+            OLED_Clear();
+			Delay_ms(10);
+			OLED_ShowString(1, 1, "老子最NB");
+			break;
+		   }
+		   case 4:
+		   {   
+            OLED_Clear();
+			Delay_ms(10);
+			OLED_ShowString(1, 1, "耶耶耶");
+			break;
+		   }
+        }
+	}
+}
+
+uint8_t Key_GetNum(void)
 {
-   p0=0x7f
-   delay_ms(500)
-   p0=0xbf
-   delay_ms(500)
-   p0=0xdf
-   delay_ms(500)
-   p0=0xef
-   delay_ms(500)
-   p0=0xf7
-   delay_ms(500)
-   p0=0xfb
-   delay_ms(500)
-   p0=0xfd
-   delay_ms(500)
-   p0=0xfe
-   delay_ms(500)
-}    
-    return 0;
+	uint8_t KeyNum = 0;		
+	
+	if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_1) == 0)			
+	{
+		Delay_ms(20);											
+		while (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_1) == 0);	
+		Delay_ms(20);											
+		KeyNum +=;											
+	}
+	return KeyNum;
 }
